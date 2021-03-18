771d57aedf8b639de5b71249e76285d2e0c11809
Dan Fingal-Surma <dfingal@gmail.com>
2021-03-18 01:23:40 -0700

    Add tools directory

---
+++ tools/run_tests.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Runs all the tests in the project.
+set -e
+
+source .venv/bin/activate
+pytest
---
+++ tools/setup_venv.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Initialize the virtual environment for running a local server, tests, and tools.
+set -e
+
+python -m venv .venv
+source .venv/bin/activate
+python -m pip install -r src/requirements.txt
+python -m pip install -r src/requirements-test.txt
