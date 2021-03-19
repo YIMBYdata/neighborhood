41d363abe83b33b3b740b8085103549d3775c0ba
Dan Fingal-Surma <dfingal@gmail.com>
2021-03-18 23:15:58 -0700

    Update setup_venv.sh

--- tools/setup_venv.sh
+++ tools/setup_venv.sh
@@ -4,6 +4,6 @@ set -e
 
 python -m venv .venv
 source .venv/bin/activate
-python -m pip install --upgrade pip
+python -m pip install --upgrade pip wheel
 python -m pip install -r src/requirements.txt
 python -m pip install -r src/requirements-test.txt
