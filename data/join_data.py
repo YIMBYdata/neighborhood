38e07e8db39479b3d77c6fae056f63d06b789b62
Dan Fingal-Surma <dfingal@gmail.com>
2021-05-05 23:04:14 -0700

    Reimplement join_data using pandas

--- data/join_data.py
+++ data/join_data.py
@@ -11,58 +11,34 @@ Sample usage:
   ./join_data.py elections-data.txt precincts.tsv > ../src/data/neighborhood_data.tsv
 """
 
-import csv
 import sys
 
+import pandas as pd
 
-def join_election_data(election_filename, precinct_filename):
+OUTPUT_COLUMNS = [
+    "StreetName",
+    "StreetType",
+    "SideCode",
+    "HouseNumLo",
+    "HouseNumHi",
+    "District",
+    "Neighborhood",
+]
+
+
+def join_election_data(election_filename: str, precinct_filename: str) -> None:
     """Joins the two datasets and outputs to stdout."""
-    # Read in the precinct data.
-    precinct_data = {}
-    with open(precinct_filename) as precinct_fd:
-        reader = csv.reader(precinct_fd, delimiter="\t")
-        for precinct, district, neighborhood in reader:
-            assert precinct not in precinct_data
-            precinct_data[precinct] = district, neighborhood
-
-    # Read in the elections data and perform a join, filtering to only keep
-    # the fields that find_neighborhood.py needs and normalizing the data to
-    # lowercase.
-    output = []
-    with open(election_filename) as election_fd:
-        reader = csv.DictReader(election_fd, delimiter="\t")
-        for row in reader:
-            street_name = row["StreetName"].lower()
-            if street_name.startswith("@"):
-                continue  # Filter out some weird records.
-            district, neighborhood = precinct_data[row["PrecinctID"]]
-            output.append(
-                [
-                    street_name,
-                    row["StreetType"].lower(),
-                    row["SideCode"].upper(),
-                    row["HouseNumLo"],
-                    row["HouseNumHi"],
-                    district,
-                    neighborhood,
-                ]
-            )
-
-    # Sort the output so it's easier for a human to parse.
-    writer = csv.writer(sys.stdout, delimiter="\t")
-    writer.writerow(
-        [
-            "StreetName",
-            "StreetType",
-            "SideCode",
-            "HouseNumLo",
-            "HouseNumHi",
-            "District",
-            "Neighborhood",
-        ]
-    )
-    for row in sorted(output):
-        writer.writerow(row)
+    election_data = pd.read_csv(election_filename, "\t", low_memory=False)
+    precinct_data = pd.read_csv(precinct_filename, "\t")
+    assert precinct_data["PrecinctID"].is_unique
+    data = pd.merge(election_data, precinct_data, on="PrecinctID", how="inner")
+    # Filter out weird records
+    data = data[~data["StreetName"].str.startswith("@")]
+    data["StreetName"] = data["StreetName"].str.lower()
+    data["StreetType"] = data["StreetType"].str.lower()
+    data["SideCode"] = data["SideCode"].str.upper()
+    data.sort_values(by=OUTPUT_COLUMNS, inplace=True)
+    data.to_csv(sys.stdout, sep="\t", index=False, columns=OUTPUT_COLUMNS)
 
 
 if __name__ == "__main__":
